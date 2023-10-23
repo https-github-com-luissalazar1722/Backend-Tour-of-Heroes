d198e1587e07852c2f3875cfc11cabed6488a09d
luissalazar1722 <luis.salazarr@udea.edu.co>
2023-10-22 21:46:08 -0500

    Realizando actividad

---
+++ backend-heroes/src/main/java/com/demo/heroes/controller/HeroController.java
@@ -0,0 +1,65 @@
+package com.demo.heroes.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.demo.heroes.model.Hero;
+import com.demo.heroes.service.HeroService;
+
+@Controller
+@CrossOrigin("*")
+@RequestMapping(value = "")
+public class HeroController {
+	static Logger log = LoggerFactory.getLogger(HeroController.class.getName());
+
+	@Autowired
+	HeroService heroService;
+	
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public ResponseEntity<String> test() {
+		return new ResponseEntity<String>("Service is running now.", HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public ResponseEntity<Hero> createHero(@RequestBody Hero request) {
+		Hero response = heroService.save(request);
+		return new ResponseEntity<Hero>(response, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/update", method = RequestMethod.PUT)
+	public ResponseEntity<Hero> updateHero(@RequestBody Hero request) {
+		Hero response = heroService.save(request);
+		return new ResponseEntity<Hero>(response, HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<String> deleteHero(@PathVariable long id) {
+		if (heroService.delete(id)) {
+			return new ResponseEntity<String>("Delete successfully id: " + id, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<String>("Ops! There are something wrong with deletion id: " + id, HttpStatus.OK);
+		}
+	}
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ResponseEntity<List<Hero>> list() {
+		return new ResponseEntity<List<Hero>>(heroService.list(), HttpStatus.OK);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Hero> detail(@PathVariable long id) {
+		return new ResponseEntity<Hero>(heroService.getDetailById(id), HttpStatus.OK);
+	}
+
+}
---
+++ backend-heroes/src/main/java/com/demo/heroes/service/HeroService.java
@@ -0,0 +1,17 @@
+package com.demo.heroes.service;
+
+import java.util.List;
+
+import com.demo.heroes.model.Hero;
+
+public interface HeroService {
+	Hero save(Hero hero);
+	
+	Hero update(Hero hero) throws Exception;
+	
+	boolean delete (long id);
+	
+	List<Hero> list();
+	
+	Hero getDetailById(long id);
+}
